7526d18a5a60576b7644d865e5a64f6d7cd3ff78
virajlakade <lakadeviraj09@gmail.com>
2026-07-01 13:36:59 +0530

    Controller for Attendance and ASubject done

---
+++ src/main/java/com/viraj/projectbackend/controller/Attendence/AttendanceController.java
@@ -0,0 +1,52 @@
+package com.viraj.projectbackend.controller.Attendence;
+
+
+
+import com.viraj.projectbackend.model.Attendence.AttendanceLog;
+import com.viraj.projectbackend.service.AttendanceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/attendance")
+@CrossOrigin(origins = "http://localhost:5173")
+public class AttendanceController {
+
+    @Autowired
+    private AttendanceService attendanceService;
+
+    @GetMapping
+    public List<AttendanceLog> getAllAttendance() {
+        return attendanceService.getAllAttendance();
+    }
+
+    @GetMapping("/subject/{subjectId}")
+    public List<AttendanceLog> getAttendanceBySubject(
+            @PathVariable Long subjectId) {
+
+        return attendanceService.getAttendanceBySubject(subjectId);
+    }
+
+    @PostMapping
+    public AttendanceLog addAttendance(
+            @RequestBody AttendanceLog attendanceLog) {
+
+        return attendanceService.addAttendance(attendanceLog);
+    }
+
+    @PutMapping("/{id}")
+    public AttendanceLog updateAttendance(
+            @PathVariable Long id,
+            @RequestBody AttendanceLog attendanceLog) {
+
+        return attendanceService.updateAttendance(id, attendanceLog);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAttendance(@PathVariable Long id) {
+        attendanceService.deleteAttendance(id);
+    }
+
+}
