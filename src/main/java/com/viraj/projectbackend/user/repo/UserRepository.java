c4c15470277b121fe96e12b949f1dbd7b4691a72
virajlakade <lakadeviraj09@gmail.com>
2026-07-11 22:21:49 +0530

    REpolayer Done

---
+++ src/main/java/com/viraj/projectbackend/user/repo/UserRepository.java
@@ -0,0 +1,16 @@
+package com.viraj.projectbackend.User.repository;
+
+import com.viraj.projectbackend.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+}
