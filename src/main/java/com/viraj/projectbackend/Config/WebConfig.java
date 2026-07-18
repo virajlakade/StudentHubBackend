a4f7bc3f6fb53d7e2d271ffc873f3353ce72eff3
virajlakade <lakadeviraj09@gmail.com>
2026-07-18 18:49:37 +0530

    Email verify done in lnf

--- src/main/java/com/viraj/projectbackend/Config/WebConfig.java
+++ src/main/java/com/viraj/projectbackend/Config/WebConfig.java
@@ -4,13 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
+        Path uploadPath = Paths.get("uploads").toAbsolutePath().normalize();
+
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
+                .addResourceLocations("file:" + uploadPath.toString() + "/");
     }
 }
